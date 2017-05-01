05c8987702af2533ea86471e203a71885c24143d
Erick Garcia <erick.m.garcia.vega@gmail.com>
2017-05-01 12:01:58 -0500

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -24,6 +24,17 @@ io.on('connection', (socket) => {
   // socket.on('createEmail', (newEmail) => {
   //   console.log('createEmail', newEmail);
   // });
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New User joined',
+    createdAt: new Date().getTime()  
+  });
 
   socket.on('createMessage', (newMessage) => {
     console.log('createMessage', newMessage);
@@ -32,6 +43,11 @@ io.on('connection', (socket) => {
       text: newMessage.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: newMessage.from,
+    //   text: newMessage.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
